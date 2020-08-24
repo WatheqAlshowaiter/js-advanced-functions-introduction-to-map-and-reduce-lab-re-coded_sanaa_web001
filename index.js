8bbc1a6ec97ab99bebc0918299fc8a77bb20ed4e
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-24 13:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+// Your code here
 
+// mapToNegativize returns an array with all values made negative
 function mapToNegativize(sourceArray) {
   return sourceArray.map((n) => n * -1);
 }
 
 // mapToNoChange returns an array with the original values
-function mapToNoChange(sourceArray){
-    return sourceArray.map((n) => n);
+function mapToNoChange(sourceArray) {
+  return sourceArray.map((n) => n);
+}
+
+// mapToDouble returns an array with the original values multiplied by 2
+function mapToDouble(sourceArray) {
+  return sourceArray.map((n) => n * 2);
+}
+
+// mapToSquare returns an array with the original values squared
+function mapToSquare(sourceArray) {
+  return sourceArray.map((n) => n * n);
 }
