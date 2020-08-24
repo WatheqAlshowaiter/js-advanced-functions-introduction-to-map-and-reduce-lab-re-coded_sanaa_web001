2cc999b3513c1f1dae7854bcb48ea6e58fba6afd
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-24 13:50:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function mapToNegativize(sourceArray) {
   return sourceArray.map((n) => n * -1);
 }
+
+// mapToNoChange returns an array with the original values
+mapToNoChange(sourceArray){
+    return sourceArray.map((n) => n);
+}
